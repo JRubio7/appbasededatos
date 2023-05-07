b8cd05be01484671ad831e9cafcf1b02d76bb8f4
JRubio7 <i202114280@cibertec.edu.pe>
2023-05-07 09:55:56 -0500

    clase semana06

--- src/main/java/pe/edu/cibertec/appbasededatos/controller/EstadoController.java
+++ src/main/java/pe/edu/cibertec/appbasededatos/controller/EstadoController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.cibertec.appbasededatos.model.bd.Estado;
 import pe.edu.cibertec.appbasededatos.service.EstadoService;
 
 @Controller
@@ -23,4 +26,33 @@ public class EstadoController {
 		return "Estado/frmMantEstado";
 		
 	}
+	
+	@GetMapping("/frmRegEstado")
+	public String frmRegEstado(Model model) {
+		model.addAttribute("estadoForm", new Estado());
+		model.addAttribute("visualizar", false);
+		
+		return "Estado/frmRegEstado"; //retorno de la vista
+	}
+	
+	//Registro
+	@PostMapping("/frmRegEstado")
+	public String registrarEstado(@ModelAttribute("estadoForm") Estado estado, Model model){
+		
+		String mensaje = "Estado registrado correctamente";
+		try {
+			estadoService.registrarEstado(estado);
+		} catch (Exception e) {
+			mensaje = "Estado no registrado";
+		}
+		
+		model.addAttribute("estadoForm", new Estado());
+		model.addAttribute("visualizar", true);
+		model.addAttribute("respuesta", mensaje);
+		
+		return "Estado/frmRegEstado";
+		
+	}
+	
+	
 }
